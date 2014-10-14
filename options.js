57849c3aab10fb1ddf50156372c6778b88754bcb
Bednarik Boldizsar <bbednarik@gmail.com>
2014-10-14 17:19:14 +0200

    Initial commit, very minimal stuff, basically just some css tweaks and default priority function

---
+++ options.js
@@ -0,0 +1,38 @@
+var btd_default_priority_start = 2;
+
+function loadDefaultPriority(){
+
+	var default_priority = localStorage['default_priority'];
+
+	if(default_priority == undefined)
+	{
+		return btd_default_priority_start;
+	}
+	else
+	{
+		return default_priority;
+	}
+}
+
+function saveDefaultPriority()
+{
+	localStorage['default_priority'] = $('#priority').val();
+}
+
+function loadOptions()
+{
+	$('#priority').val(loadDefaultPriority());
+}
+
+function saveOptions()
+{
+	saveDefaultPriority();
+	alert('Saved!');
+}
+
+$(document).ready(function(){
+	$('#saveButton').click(function(){
+		saveOptions();
+	});
+	loadOptions();
+});
